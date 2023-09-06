dbf8851fed0d662fb7eb7c78d071e0b077c1382b
Ayeza <ayezabashir442@gmail.com>
2023-09-07 00:00:06 +0500

    navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,8 +1,31 @@
+import { PiGridFour } from 'react-icons/pi'
+import { AiFillCaretDown } from 'react-icons/ai'
 const Navbar = () => {
     return (
-        <div>
+        <header className="navbar">
+            <div className="container">
+                <div className="categories">
+                    <PiGridFour className='icon' />
+                    <h4>Categories</h4>
+                    <AiFillCaretDown className='icon' />
+                </div>
+                <div className="navlink">
+                    <ul className="nav">
+                        <li>
+                            <Link to='/'>home</Link>
+                        </li>
+                        <li>
+                            <Link to='/'>home</Link>
 
-        </div>
+                        </li>
+                        <li>
+                            <Link to='/'>home</Link>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     )
 }
 
