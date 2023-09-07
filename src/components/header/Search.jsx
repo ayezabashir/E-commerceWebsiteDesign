852c3e2dd6accad4d023ec79792d22a1708728f9
Ayeza <ayezabashir442@gmail.com>
2023-09-07 15:51:05 +0500

    search component

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,8 +1,29 @@
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.webp'
+import { BiSearchAlt2, BiSolidUser, BiCart } from 'react-icons/bi'
 const Search = () => {
     return (
-        <div>
-            search
-        </div>
+        <section className="search">
+            <div className="container">
+                <div className="logo">
+                    <img src={logo} alt='logo' />
+                </div>
+                <div className="search-box">
+                    <BiSearchAlt2 className='icon' />
+                    <input type="text" placeholder='Search here...' />
+                    <span>All Category</span>
+                </div>
+                <div className="icons">
+                    <BiSolidUser className='icon' />
+                    <div className="cart">
+                        <Link to='/cart'>
+                            <BiCart className='icon' />
+                            <span>0</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
