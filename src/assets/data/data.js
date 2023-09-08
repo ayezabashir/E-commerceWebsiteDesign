663962f500fb140891e46c8c5e25acce01bea5d3
Ayeza <ayezabashir442@gmail.com>
2023-09-08 23:59:28 +0500

    category component

---
+++ src/assets/data/data.js
@@ -0,0 +1,7 @@
+export const cat = [
+    {
+        id: 1,
+        catImg: '',
+        catName: 'Fashion'
+    }
+]
--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,7 +1,18 @@
+import { cat } from "../../assets/data/data"
+
 const Categories = () => {
     return (
-        <div>
-            <h1>Categories</h1>
+        <div className="container">
+            <div className="category">
+                {
+                    cat.map(item => (
+                        <div className="box" key={item.id}>
+                            <img src={item.catImg} alt="" />
+                            <span>{item.catName}</span>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
