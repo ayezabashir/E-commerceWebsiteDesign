812ad0903d8ca58e497d0a714250244e111a8269
Ayeza <ayezabashir442@gmail.com>
2023-09-08 23:06:52 +0500

    head styles fixed

--- src/components/header/Head.jsx
+++ src/components/header/Head.jsx
@@ -4,10 +4,14 @@ const Head = () => {
         <section className="head">
             <div className="container">
                 <div className="left">
-                    <BsTelephoneFill className='icon' />
-                    <label>+123 45678-90123</label>
-                    <BsFillEnvelopeFill className='icon' />
-                    <label>ayezabashir46@gmail.com</label>
+                    <div className="group">
+                        <BsTelephoneFill className='icon' />
+                        <label>+123 45678-90123</label>
+                    </div>
+                    <div className="group">
+                        <BsFillEnvelopeFill className='icon' />
+                        <label>ayezabashir46@gmail.com</label>
+                    </div>
                 </div>
                 <div className="right">
                     <label>FAQs</label>
