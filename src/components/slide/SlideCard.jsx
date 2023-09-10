4ddbead9ade7f6fa01f4ed899f9d4cf49c6de0e1
Ayeza <ayezabashir442@gmail.com>
2023-09-10 16:21:16 +0500

    slide component

--- src/components/slide/SlideCard.jsx
+++ src/components/slide/SlideCard.jsx
@@ -1,6 +1,41 @@
+import { slider } from "../../assets/data/data"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 const SlideCard = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        appendDots: (dots) => {
+            return <ul style={{ margin: "0" }}>{dots}</ul>
+        },
+        pauseOnHover: true
+    };
     return (
         <>
+            <Slider {...settings}>
+                {
+                    slider.map(item => (
+                        <div className="box" key={item.id}>
+                            <div className="left">
+                                <h1>{item.title}</h1>
+                                <p>{item.desc}</p>
+                                <button className="btn-primary">Visit Collection</button>
+                            </div>
+                            <div className="right">
+                                <img src={item.cover} alt="" />
+                            </div>
+                        </div>
+                    ))
+                }
+            </Slider>
         </>
     )
 }
--- src/components/slide/Slider.jsx
+++ src/components/slide/Slider.jsx
@@ -3,10 +3,9 @@ import SlideCard from "./SlideCard"
 const Slider = () => {
     return (
         <div className="slider">
-            <div className="container">
-                <SlideCard />
-            </div>
+            <SlideCard />
         </div>
+
     )
 }
 
