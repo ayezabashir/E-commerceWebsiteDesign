bb00b60f41d8b557dd7d0d5266da3d006bdc83cf
Ayeza <ayezabashir442@gmail.com>
2023-09-15 20:12:17 +0500

    top catergories

--- src/components/top/TopCategory.jsx
+++ src/components/top/TopCategory.jsx
@@ -1,9 +1,18 @@
+import { PiGridFour } from 'react-icons/pi'
+import { AiFillCaretRight } from 'react-icons/ai'
 const TopCategory = () => {
     return (
         <section className="topCat ">
             <div className="container">
                 <div className="heading">
-                    <div className="heading-left"></div>
+                    <div className="heading-left">
+                        <PiGridFour className='icon' />
+                        <h2>Top Categories</h2>
+                    </div>
+                    <div className="heading-right">
+                        <span>View All</span>
+                        <AiFillCaretRight className='icon' />
+                    </div>
                 </div>
             </div>
 
