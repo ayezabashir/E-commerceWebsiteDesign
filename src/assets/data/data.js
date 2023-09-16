9fe30979d1dcb72c8c1017d90f973f27af216006
Ayeza <ayezabashir442@gmail.com>
2023-09-16 22:44:00 +0500

    new Arrivals

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -162,3 +162,42 @@ export const Topdata = [
         desc: "6k orders this week",
     },
 ]
+
+export const NewArrivaldata = [
+    {
+        id: 1,
+        cover: "./images/arrivals/arrivals1.png",
+        name: "Sunglass",
+        price: "150",
+    },
+    {
+        id: 2,
+        cover: "./images/arrivals/arrivals2.png",
+        name: "Makeup",
+        price: "250",
+    },
+    {
+        id: 3,
+        cover: "./images/arrivals/arrivals3.png",
+        name: "Smart Watch",
+        price: "50",
+    },
+    {
+        id: 4,
+        cover: "./images/arrivals/arrivals4.png",
+        name: "Lipstick",
+        price: "15",
+    },
+    {
+        id: 5,
+        cover: "./images/arrivals/arrivals5.png",
+        name: "Green Plant",
+        price: "10",
+    },
+    {
+        id: 6,
+        cover: "./images/arrivals/arrivals6.png",
+        name: "Bonsai tree",
+        price: "400",
+    },
+]
---
+++ src/components/newArrivals/NewArrivals.jsx
@@ -0,0 +1,22 @@
+import { AiFillCaretRight } from 'react-icons/ai'
+
+const NewArrivals = () => {
+    return (
+        <section className="new-arrivals">
+            <div className="container">
+                <div className="heading">
+                    <div className="heading-left">
+                        <img src='https://img.icons8.com/glyph-neue/64/26e07f/new.png' />
+                        <h2>New Arrivals</h2>
+                    </div>
+                    <div className="heading-right">
+                        <span>View All</span>
+                        <AiFillCaretRight className='icon' />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default NewArrivals
