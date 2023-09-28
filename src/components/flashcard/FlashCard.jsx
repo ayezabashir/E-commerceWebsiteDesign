6ca7c67f1b4e5c34e43731726b8305f35560b516
Ayeza <ayezabashir442@gmail.com>
2023-09-28 22:42:04 +0500

    flashcard

--- src/components/flashcard/FlashCard.jsx
+++ src/components/flashcard/FlashCard.jsx
@@ -39,6 +39,32 @@ const FlashCard = ({ productItems, addToCart }) => {
         speed: 2000,
         nextArrow: <NextArrow />,
         prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
 
     };
     return (
