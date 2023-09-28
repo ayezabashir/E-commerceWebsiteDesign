499f6d298baeb32937de7404e46ffbe19cd81a30
Ayeza <ayezabashir442@gmail.com>
2023-09-28 22:48:46 +0500

    discount card

--- src/components/discount/DiscountCard.jsx
+++ src/components/discount/DiscountCard.jsx
@@ -10,6 +10,31 @@ const DiscountCard = () => {
         slidesToScroll: 1,
         autoplay: true,
         margin: 200,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
     return (
         <>
