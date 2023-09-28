430da57f186987696117dda2851d03f4e4e77e60
Ayeza <ayezabashir442@gmail.com>
2023-09-28 22:46:42 +0500

    topcart

--- src/components/top/TopCart.jsx
+++ src/components/top/TopCart.jsx
@@ -8,6 +8,23 @@ const TopCart = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     };
     return (
         <>
